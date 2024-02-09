18b6bc2894d2baa807e2f121992fd4e644f552ad
DESKTOP-S98238J\Raquel <Raquel Rodriguez Gonzalez>
2024-02-09 19:37:02 +0100

    configuración de cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const routes = require("./routes/index");
 const swaggerUi = require("swagger-ui-express");
@@ -8,10 +9,15 @@ const mongoConnect = require("./config/mongo");
 require("dotenv").config();
 
 const swaggerDocs = swaggerJsDoc(swaggerConfig);
+
+app.use(cors());
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use("/", routes);
 
-app.listen(process.env.port | 3000, () => {
-    console.log("Example app listening on port 3000!");
+
+
+app.listen(process.env.port | 4000, () => {
+    console.log("Example app listening on port 4000!");
     mongoConnect();
 });
