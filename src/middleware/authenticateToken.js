0bbc5c59f852b3391064e525ccefbe259d08d58f
DESKTOP-S98238J\Raquel <Raquel Rodriguez Gonzalez>
2024-03-01 14:14:22 +0100

    corregir variable

--- src/middleware/authenticateToken.js
+++ src/middleware/authenticateToken.js
@@ -4,7 +4,7 @@ function authenticateToken(req, res, next) {
   const token = req.header('Authorization');
   if (!token) return res.status(401).json({ error: 'Authentication failed' });
 
-  let tokenClean = toke.replace.replace('Bearer ', '').replace('bearer ', '');
+  let tokenClean = token.replace.replace('Bearer ', '').replace('bearer ', '');
 
   jwt.verify(tokenClean, process.env.token, (err, user) => {
     if (err) return res.status(403).json({ error: 'Token is not valid' });
