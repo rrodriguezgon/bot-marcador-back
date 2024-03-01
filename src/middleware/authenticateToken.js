8cda4420b06fefdfcc045d042b595dbe0e20b40e
DESKTOP-S98238J\Raquel <Raquel Rodriguez Gonzalez>
2024-03-01 14:12:26 +0100

    quitar bien bearer

--- src/middleware/authenticateToken.js
+++ src/middleware/authenticateToken.js
@@ -4,7 +4,9 @@ function authenticateToken(req, res, next) {
   const token = req.header('Authorization');
   if (!token) return res.status(401).json({ error: 'Authentication failed' });
 
-  jwt.verify(token.replace('Bearer ', ''), process.env.token, (err, user) => {
+  let tokenClean = toke.replace.replace('Bearer ', '').replace('bearer ', '');
+
+  jwt.verify(tokenClean, process.env.token, (err, user) => {
     if (err) return res.status(403).json({ error: 'Token is not valid' });
     req.user = user;
     next();
